26ddf7384508949296d4678dfb84c79e6acc3a57
Allen Royston <allen.acr@gmail.com>
2016-07-29 09:14:21 -0700

    Update semantic-ui.directive.ts

--- dist/semantic-ui.directive.ts
+++ dist/semantic-ui.directive.ts
@@ -254,6 +254,35 @@ export class uiSemanticPopup {
 
 }
 
+@Directive({
+  selector: '[ui-shape]',
+  host: {
+    '(click)':      'onClick()'
+  }
+})
+export class uiSemanticShape {
+  @Input('options') options:any
+
+  onClick(){
+    var i = this;
+
+    if(i.options == undefined){
+      i.options = {selector: '.shape', animation: 'flip up'}
+    }
+    else{
+      if(i.options.selector == undefined){
+        i.options.selector = '.shape'
+      }
+      if(i.options.selector == undefined){
+        i.options.animation = '.flip up'
+      }
+    }
+    $(i.options.selector).shape(i.options.animation)
+  }
+
+}
+
+
 
 @Directive({
   selector: '[ui-tab]'
